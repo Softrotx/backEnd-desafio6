aa86a769f06cc494eade7df5022cdaa2255c4333
Claudio Fuentes <69474291+Softrotx@users.noreply.github.com>
2024-04-30 22:44:17 -0400

    Delete src/config/github.private.js
    se elimina gitprivate

--- src/config/github.private.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    appId: '888031',
-    clientID: 'Iv1.510e3834e7482770',
-    clientSecret: '06a48ccf6f09abf5fd95e3a3b1173ae1edd17f67',
-    callbackURL: 'http://localhost:8080/api/sessions/githubcallback'
-}
